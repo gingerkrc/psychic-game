cfffe5cdd9154bc094c5961752cdfe088f0decf2
Ginger KRC <gingerkr@gmail.com>
2018-10-10 14:56:48 -0600

    game resets and lists guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,18 +7,19 @@ var wins = 0;
 var losses = 0;
 var lettersGuessed = [];
 var computerGuess = [];
+var guessesLeft = 10;
 
 //Reference HTML for text place holder
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
+var guessesLeftText = document.getElementById("guesses-left");
 
-
+// can make variable that is equal to this equation 
 //Select computer guess
 window.onload = function() {
 	var compGuess = computerSelection[Math.floor(Math.random() * computerSelection.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
+	console.log(compGuess);
 }
 
 // Start the game
@@ -27,28 +28,45 @@ document.onkeyup = function(event) {
 	lettersGuessed.push(userGuess);
 	console.log(userGuess[0]);
 
-if (userGuess === computerGuess[0]) {
-	wins++;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
+// If guesses are not exhausted
+if (guessesLeft > 0) {
+
+
+// Then user can accrue wins if they match the computer guess
+	if (userGuess === computerGuess[0]) {
+		wins++;
+		lettersGuessed = [];
+		computerGuess =[];
+		var compGuess = computerSelection[Math.floor(Math.random() * computerSelection.length)];
+		computerGuess.push(compGuess);
+		console.log(computerGuess[0]);
+	}
+
+// And user can lose guesses if they don't match computer guess
+	else {
+	guessesLeft--;
+	guessesLeftText.textContent = "Guesses remaining: " + guessesLeft;
+	
+	}
+
+// User loses game when they have no guesses left
+} else {
+	document.getElementById("game-loss").textContent= "You are not psychic!";
+	losses++;
+	lettersGuessed = [];
+	computerGuess.length = [];
 	var compGuess = computerSelection[Math.floor(Math.random() * computerSelection.length)];
 	computerGuess.push(compGuess);
+	guessesLeft = 10;
 	console.log(computerGuess[0]);
 }
 
-else {
-	losses++;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = computerSelection[Math.floor(Math.random() * computerSelection.length)];
-	computerGuess.push(compGuess);
-    console.log(computerGuess[0]);
-    
 
 // Display the wins, losses, and guesses
-        winsText.textContent = "Hits: " + wins;
-		lossesText.textContent = "Misses: " + losses;
-		guessesText.textContent = "Letters you have guessed: " + userGuess; 
+        winsText.textContent = "Wins: " + wins;
+		lossesText.textContent = "Losses: " + losses;
+		guessesText.textContent = "Letter guessed: " + lettersGuessed.join(", "); 
+		
+
             
-    }
 }
